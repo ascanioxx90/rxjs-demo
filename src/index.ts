80bf2f2edc1c33f0cd3ccf8e223b52f983bde593
skyascanioAn <antonio.ascanio.nttdata@skytv.it>
2022-06-07 18:06:04 +0200

    subject examples added

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,4 @@ import { ReplaySubjectDemo } from './subjects/replay-subject';
 import { GreetingsDemo } from './subjects/greetings';
 import { SubjectVsObservableDemo } from './subjects/subject-vs-observable';
 
-SubjectVsObservableDemo.main();
+SubjectDemo.main();
